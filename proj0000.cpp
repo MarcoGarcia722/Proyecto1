1769a24f5f8cd836871ce5298764a6b15dc71962
JonathanVeloz730 <kapribby@hotmail.com>
2015-07-10 12:11:45 -0500

    Casos
    Aumento de la selección d e opciones.

--- proj0000.cpp
+++ proj0000.cpp
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <conio.h>
-#include <iostream>
+
+
 main()
 {
- int op;
+clrscr();
+ int op; op1
+ char hora[20],
+ printf("\t bienvenidos:");
  printf("\t \t \n Menu de opciones:");
  printf("\n 1.Ingresar una nueva hora de clases:");
  printf("\n 2.Ver el horario de hoy:");
  printf("\n 3.Horario de la semana:");
  printf("\n 4.Salir:");
  printf("\n Porfavor ingrese la opcion que desea:");
- getch();
- clrscr();
- printf("\t bienvenidos:");
+
  switch(op)
  {
   case 1:
-        printf("\n Horario:");
+        printf("\n Ingrese una nueva hora de clases:");
+
   case 2:
-        printf("\n Profesiones:");
+
+        printf("\n horario:");
+        printf("\n escoja el dia:") ;
+        prinf("\n 1.Lunes");
+        prinf("\n 2.Martes");
+        prinf("\n 3.Miercoles");
+        prinf("\n 4.Jueves");
+        prinf("\n 5.Viernes");
+        switch(op1)
+        {
+         case 1:
+         		printf("\n\nLunes:\n\nQuimica\nMAtematicaI\FisicaI\nIngles\nMEtodologiaa\n\n");break;
+         case 2:
+         		:printf("\n\nMArtes:\n\nIngles\nDeportes\nProgramacion\nFisica\nMatematicas\n\n");break;
+         case 3:
+         		:printf("\n\nMiercoles:\n\nAlgebra\nQuimica\nMEtodologia\nProgramacion\nMatematicas\nFisica\n\n");break;
+         case 4:
+         		:printf("\n\nJueves:\n\nMAtematica\nIngles\nAlgebra\nFisica\nQuimica \n\n");break;
+         case 5:
+         		:printf("\n\nLunes:\n\n}PRogramacion\nAlgebra\nIngles\nMatematicas\nFisica\n\n");break;
+        }
+
   default:
   printf("No tiene ninguna opcion:");
  }
