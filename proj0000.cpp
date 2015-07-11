08c8dce88f41026ab6997afc5a347e160a43bee3
MarcoGarcia722 <marcogarcia241@gmail.com>
2015-07-10 23:09:02 -0500

    prueba(sin calificar)
    solo prueba

--- proj0000.cpp
+++ proj0000.cpp
@@ -4,15 +4,16 @@
 main()
 {
  int op;
+ clrscr();
+ printf("Prueba de github:");
  printf("\t \t \n Menu de opciones:");
  printf("\n 1.Ingresar una nueva hora de clases:");
  printf("\n 2.Ver el horario de hoy:");
  printf("\n 3.Horario de la semana:");
  printf("\n 4.Salir:");
  printf("\n Porfavor ingrese la opcion que desea:");
- getch();
- clrscr();
  printf("\t bienvenidos:");
+ printf("Prueba de github:");
  switch(op)
  {
   case 1:
@@ -22,4 +23,5 @@ main()
   default:
   printf("No tiene ninguna opcion:");
  }
+  getch();
 }
