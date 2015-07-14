b5e9b60929587845bc376eb20c3413bb32b12bee
JonathanVeloz730 <kapribby@hotmail.com>
2015-07-13 20:10:19 -0500

    Función ingresarHoraClase
    Implementación de función ingresarHoraClase donde ingresamos en el
    Struct los datos de la hora clase

--- proj0000.cpp
+++ proj0000.cpp
@@ -27,6 +27,17 @@ int menu()
 hora_clase ingresarHoraClase()
 {
       struct hora_clase hora;
+      printf("\t Por favor ingrese los siguientes datos:");
+      printf("\n Nombre de la Materia:");
+      scanf("%s",hora.nombre_materia);
+      printf("\n Nombre del Semestre:");
+      scanf("%s",hora.nombre_semestre);
+      printf("\n Día de la Semana:");
+      scanf("%s",hora.dia_semana);
+      printf("\n Hora Inicio:");
+      scanf("%s",hora.hora_inicio);
+      printf("\n Hora Fin:");
+      scanf("%s",hora.hora_fin);
       return hora;
 }
 
@@ -54,40 +65,10 @@ main()
  opcion = menu();
  switch(opcion){
   case 1:
-        printf("\n Ingrese una nueva hora de clases:");
-        scanf("%c",&h);
-        switch(h)
-        {
-         case 1:
-         printf("07H00-08H00");
-         break;
-         case 2:
-         printf("08H00-09H00");
-         break;
-         case 3:
-         printf("09H00-10H00");
-         break;
-         case 4:
-         printf("10H00-11H00");
-         break;
-         case 5:
-         printf("11H00-12H00");
-         break;
-         case 6:
-         printf("12H00-13H00");
-         break;
-         case 7:
-         printf("13H00-14H00""ALMUERZO");
-         break;
-         case 8:
-         printf("14H00-15H00");
-         break;
-         case 9:
-         printf("15H00-16H00");
-         break;
-
-        }
-
+       struct hora_clase item;
+       item = ingresarHoraClase();
+       printf("%s",item.nombre_materia);     // prueba de ingreso
+       break;
   case 2:
 
        subOpcion = subMenuDiasSemana();
